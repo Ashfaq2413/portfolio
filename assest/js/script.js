0dea2781d3c5ee0e45c58c326388694cbeb4c6ac
Ashfaq2413 <mjaa241330@gmail.com>
2024-03-16 10:38:22 +0530

    nav bar

--- assest/js/script.js
+++ assest/js/script.js
@@ -30,4 +30,9 @@ window.onscroll = () => {
     header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    // rempve toggle icon and navbar when click navbar links(scroll)
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
