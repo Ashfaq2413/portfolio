ae776ce98b0d5f1264792563d0aab4440c656e72
Ashfaq2413 <mjaa241330@gmail.com>
2024-03-17 23:24:51 +0530

    fixed toggle button and nav bar

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,38 +1,65 @@
-// Toggle icon navbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+(function(){
 
-menuIcon.onclick =()=>{
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+    $(document).ready(function() {
+        // Toggle icon navbar
+        var topMenu = $('.jq-top-nav-link');
 
-//scroll sections
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+        // $('.jq-top-nav-link').click(function(){
+        //     $('.jq-top-nav-link').removeClass('active');
+        //     $(this).addClass('active');
+        // });
 
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.screenY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+        $('#menu-icon').click(function(){
+            $('.navbar').toggleClass('active');
+        });
 
-        if(top >= offset && top < offset + height){
-            //active navbar links
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
+        // Cache the navigation links
+        var $navigationLinks = $('.jq-top-nav-link');
+        
+        // Cache (offset, height) pairs for sections
+        var sectionOffsets = [];
+        $('section').each(function() {
+          sectionOffsets.push({
+            id: $(this).attr('id'),
+            offset: $(this).offset().top,
+            height: $(this).height()
+          });
+        });
+        
+        // Function to check which section is in view
+        function checkSectionInView() {
+          // Get current scroll position
+          var scrollPosition = $(window).scrollTop();
+          
+          // Iterate through each section and check if it's in view
+          $.each(sectionOffsets, function() {
+            var sectionOffset = this.offset;
+            var sectionHeight = this.height;
+            
+            if (scrollPosition >= sectionOffset && scrollPosition < sectionOffset + sectionHeight) {
+              // If section is in view, add active class to corresponding navigation link
+              $navigationLinks.removeClass('active');
+              $('.jq-top-nav-link[href="#' + this.id + '"]').addClass('active');
+            }
+
+          });
         }
-    });
-
-    header = document.querySelector('header');
-
-    header.classList.toggle('sticky', window.scrollY > 100);
-
-    // rempve toggle icon and navbar when click navbar links(scroll)
-
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
-}
+        
+        // Check section in view when page loads
+        // checkSectionInView();
+        
+        // Check section in view when window is scrolled
+        $(window).scroll(function() {
+            checkSectionInView();
+            if ($(this).scrollTop() > 30) {
+                // Change background color if scrolled 50px down
+                $('header').css('background-color', 'var(--bg-color)');
+            } else {
+                // Make background transparent if not scrolled 50px down
+                $('header').css('background-color', 'transparent');
+            }
+        });
+      });
+      
+    
+})();
